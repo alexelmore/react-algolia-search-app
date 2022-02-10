26c7c8bfa848d8affa9254208379278398ff63aa
Alex Elmore <aelmore@mrk-c02gf1f1md6r.lan>
2022-02-10 09:10:44 -0600

    update titles

--- algolia-react/src/components/Search.jsx
+++ algolia-react/src/components/Search.jsx
@@ -1,5 +1,4 @@
 import algoliasearch from "algoliasearch/lite";
-
 import {
   InstantSearch,
   Hits,
@@ -9,26 +8,25 @@ import {
   RefinementList,
   Configure,
 } from "react-instantsearch-dom";
-
 import Hit from "./Hit";
 import "../App.css";
 
+// Constants holding app id and search key for search client
 const appId = process.env.REACT_APP_ALGOLIA_APP_ID;
 const searchKey = process.env.REACT_APP_ALGOLIA_SEARCH_KEY;
-
 // Create const holding my search client
 const searchClient = algoliasearch(appId, searchKey);
-console.log(appId, searchKey);
+
 function Search() {
   return (
     <div className="ais-InstantSearch">
-      <h1>React InstantSearch e-commerce</h1>
+      <h1>React InstantSearch App</h1>
       <InstantSearch indexName="demo_ecommerce" searchClient={searchClient}>
         <div className="left-panel">
           <ClearRefinements />
           <h2>Brands</h2>
           <RefinementList attribute="brand" />
-          <Configure hitsPerPage={8} />
+          <Configure hitsPerPage={4} />
         </div>
         <div className="right-panel">
           <SearchBox />
