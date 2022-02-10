800b05c3bbf58fa9de7d07e34613de361a9348af
Alex Elmore <aelmore@mrk-c02gf1f1md6r.lan>
2022-02-10 07:56:48 -0600

    add algolia instant search pkg

--- algolia-react/src/App.js
+++ algolia-react/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+
+import "./App.css";
+const searchClient = algoliasearch(
+  "382ZC2MIQQ",
+  "b291b92ef1dfe915a21286a87e8e74ce"
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
+      <SearchBox />
+      <Hits />
+    </InstantSearch>
   );
 }
 
