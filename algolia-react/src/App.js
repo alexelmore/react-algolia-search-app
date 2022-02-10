a7739330cde3d836869f7bfe2a758cdfdba176b6
Alex Elmore <aelmore@mrk-c02gf1f1md6r.lan>
2022-02-10 08:17:45 -0600

    make App and Hit functional components

--- algolia-react/src/App.js
+++ algolia-react/src/App.js
@@ -1,7 +1,18 @@
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from "react-instantsearch-dom";
+import Hit from "./components/Hit";
 import "./App.css";
+
+// Create const holding my search client
 const searchClient = algoliasearch(
   "382ZC2MIQQ",
   "b291b92ef1dfe915a21286a87e8e74ce"
@@ -9,10 +20,22 @@ const searchClient = algoliasearch(
 
 function App() {
   return (
-    <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
-      <SearchBox />
-      <Hits />
-    </InstantSearch>
+    <div className="ais-InstantSearch">
+      <h1>React InstantSearch e-commerce</h1>
+      <InstantSearch indexName="demo_ecommerce" searchClient={searchClient}>
+        <div className="left-panel">
+          <ClearRefinements />
+          <h2>Brands</h2>
+          <RefinementList attribute="brand" />
+          <Configure hitsPerPage={8} />
+        </div>
+        <div className="right-panel">
+          <SearchBox />
+          <Hits hitComponent={Hit} />
+          <Pagination />
+        </div>
+      </InstantSearch>
+    </div>
   );
 }
 
---
+++ algolia-react/src/components/Hit.jsx
@@ -0,0 +1,24 @@
+import { Highlight } from "react-instantsearch-dom";
+import PropTypes from "prop-types";
+import "../App.css";
+
+function Hit(props) {
+  return (
+    <div>
+      <img src={props.hit.image} align="left" alt={props.hit.name} />
+      <div className="hit-name">
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight attribute="description" hit={props.hit} />
+      </div>
+      <div className="hit-price">${props.hit.price}</div>
+    </div>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default Hit;
